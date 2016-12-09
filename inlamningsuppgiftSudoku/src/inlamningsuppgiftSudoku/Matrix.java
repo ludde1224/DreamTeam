97c9a9a68875b8ddf0790e464918966061a2bf9e
ludde1224 <ludde1224@gmail.com>
2016-12-09 19:43:54 +0100

    Metoderna för "Matrix" tillagda

--- inlamningsuppgiftSudoku/src/inlamningsuppgiftSudoku/Matrix.java
+++ inlamningsuppgiftSudoku/src/inlamningsuppgiftSudoku/Matrix.java
@@ -1,5 +1,70 @@
 package inlamningsuppgiftSudoku;
 
 public class Matrix {
+	private SudokuNumber[][] matrix;
+
+	/**
+	 * Skapar en tom matris
+	 */
+	public Matrix() {
+
+	}
+
+	/**
+	 * Skapar en matris med innehållet i vector. Matrisen fyller först på alla
+	 * kolumner på första raden och fortsätter sedan samma mönster på nästa rad
+	 * 
+	 * @param vector
+	 *            Vektorn med SudokuNumberobjekt som matrisen ska fyllas med
+	 */
+	public Matrix(SudokuNumber[] vector) {
+
+	}
+	
+	
+	/**
+	 * Kollar om matrisen innehåller numret "number"
+	 * 
+	 * @param number
+	 * numret som ska kollas
+	 * @return
+	 * Blir sann om matrisen innehåller number
+	 */
+	public boolean contains(Integer number){
+		return false;
+	}
+
+	/**
+	 * Returnerar en vektor med raden i matrisen som specificeras av "row"
+	 * 
+	 * @param row
+	 * Specificerar vilken rad som ska returneras
+	 * @return
+	 * En vektor med SudokuNumberobjekt
+	 */
+	public SudokuNumber[] getRow(Integer row) {
+		SudokuNumber[] rowVector = new SudokuNumber[3];		
+		for (int i = 0; i<3; i++){
+			rowVector[i] = matrix[row][i];
+		}
+		return rowVector;
+	}
+	
+	/**
+	 * Returnerar en vektor med kolumnen i matrisen som specificeras av "column"
+	 * 
+	 * @param col
+	 * Specificerar vilken kolumn som ska returneras
+	 * @return
+	 * En vektor med SudokuNumberobjekt
+	 */
+	public SudokuNumber[] getCol(Integer col) {
+		SudokuNumber[] colVector = new SudokuNumber[3];		
+		for (int i = 0; i<3; i++){
+			colVector[i] = matrix[i][col];
+		}
+		return colVector;
+	}
+	
 
 }
