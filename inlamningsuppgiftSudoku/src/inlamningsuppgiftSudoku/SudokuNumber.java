fed28da033c81edf3f7b28154aeccad2710c9977
jan-akebergmark <jan-akebergmark@10.3.47.62>
2016-12-09 20:10:05 +0100

    Sod of mate

--- inlamningsuppgiftSudoku/src/inlamningsuppgiftSudoku/SudokuNumber.java
+++ inlamningsuppgiftSudoku/src/inlamningsuppgiftSudoku/SudokuNumber.java
@@ -1,5 +1,54 @@
 package inlamningsuppgiftSudoku;
 
 public class SudokuNumber {
+	private int value;
+	private boolean flag;
+	
+	/**
+	 * Skapar ett Sudokunr med värdet value
+	 * @param value Insatt värde
+	 * @param flag True om värdet är insatt av användaren
+	 */
+	public SudokuNumber(int value, boolean flag){
+		this.value = value;
+		
+	}
+
+	/**
+	 * Hämtar värdet från en ruta i Sudokut
+	 * @return Returnerar det önskade värdet
+	 */
+	
+	public int getValue(){
+		return value;
+	}
+	
+/**
+ *  Sätter ett nytt värde i Sudokut 
+ * @param value
+ * @return Returnerar det gamla värdet
+ */
+	
+	public int setValue(int value){
+		this.value = value;	
+		return this.value;
+	}
+	
+/**
+ * 
+ * @return Returnerar true om värdet är insatt av användaren, annars false
+ */
+	public boolean getFlag(){
+			return flag;
+	}
+	
+	/**
+	 * Flaggar ett Sudokunr
+	 * @return Returnerar true om värdet är insatt av användaren
+	 */
+	public boolean setFlag(){
+		flag = true;
+		return flag;
+	}
 
 }
